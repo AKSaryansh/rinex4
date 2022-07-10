b2f1c09975aeb3a55e8acf1de1fa619fd68c0766
AKSaryansh <109017517+AKSaryansh@users.noreply.github.com>
2022-07-10 16:16:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ model = joblib.load('spam-ham')
 st.title('Spam-Ham Classifier')
 ip = st.text_input('Enter your message')
 op = model.predict([ip])
-if st.button('Predict')
+if st.button('Predict'):
   st.title(op[0])
               
   
